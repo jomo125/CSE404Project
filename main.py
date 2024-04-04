0a7c6101ac86e7a6919ee87873e53ac384ab5e9c
Joey <morri806@msu.edu>
2024-04-04 14:22:39 -0400

    added email functionality

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+from dotenv import load_dotenv
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import time
+
+load_dotenv()
 
 # Path to label map file and TFLite model
 PATH_TO_LABELS = "everything_labels.txt"
 PATH_TO_MODEL = "everything_model.tflite"
 
+GMAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
+
+last_email_time = 0
+
 # Load the label map
 with open(PATH_TO_LABELS, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
+
+def send_email(subject, body):
+    # Your email credentials
+    sender_email = "josephmorrison702@gmail.com"
+    receiver_email = "josephmorrison702@gmail.com"
+    password = GMAIL_PASSWORD
+
+    # Creating the email message
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        # Connecting to the Gmail SMTP server
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(sender_email, password)
+        
+        # Sending the email and closing the connection
+        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.close()
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 
 # Load TFLite model and allocate tensors
 interpreter = tf.lite.Interpreter(model_path=PATH_TO_MODEL)
@@ -50,7 +89,16 @@ while True:
         if scores[i] > 0.75:
             object_name = labels[int(classes[i])]
             if object_name in detectable_objects:
-                print(f"Detected {object_name} with confidence {scores[i]*100:.2f}%")
+                current_time = time.time()  # Get the current time
+                # Check if 30 seconds have elapsed since the last email was sent
+                if current_time - last_email_time >= 30:
+                    # Send the email
+                    send_email("Pet Detected", f"Hello,\n\nYour {object_name} has been detected with {scores[i]*100:.2f}% confidence.\n\nSincerely,\nSmart Pet Detection")
+                    # Update the last email time to the current time
+                    print("Email sent successfully!")
+                    last_email_time = current_time
+                else:
+                    print(f"Email not sent. {30-(current_time - last_email_time):.2f} more seconds.")
 
     # Display the resulting frame
     cv2.imshow('Object Detector', frame)
